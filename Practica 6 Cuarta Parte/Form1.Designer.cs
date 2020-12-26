de9318e980cca83af582cdc948f4cc6807dc5f59
Curro Mediavilla <59694404+Curro-Medi@users.noreply.github.com>
2020-12-26 01:52:19 +0100

    Add files via upload

---
+++ Practica 6 Cuarta Parte/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Practica5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Practica5.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(699, 976);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Firebrick;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(617, 982);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aceptar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.ClientSize = new System.Drawing.Size(711, 1026);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Practica 6 Cuarta Parte/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica5
+{
+    public partial class Form1 : Form
+    {
+        MainWindow mw;
+        List<Asientos> DatosAsientos;
+        Sala sala;
+        
+        public Form1(MainWindow mw, List<Asientos> DatosAsientos, Sala sala)
+        {
+            InitializeComponent();
+            this.DatosAsientos = DatosAsientos;
+            this.mw = mw;
+            this.sala = sala;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int precio = 0;
+
+            var qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            var qrCode = qrEncoder.Encode("texto a codificar");
+            var renderer = new GraphicsRenderer(new FixedModuleSize(5,
+            QuietZoneModules.Two), Brushes.Black, Brushes.White);
+            using (var stream = new FileStream(Application.StartupPath +
+            @"\imagenes\qrcode.png", FileMode.Create))
+                renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, stream);
+            /*Para cargar la imagen de manera dinámica, primero hemos de
+            agregar un parámetro para la ruta. Luego insertamos en el informe
+            una imagen, el origen de la imagen ha de ser “externo2 y en valor
+            establecemos el parámetro.
+            Luego insertamos la ruta de la imagen en el parámetro string
+            "rutaImagen" siendo qrcode.png el código generado en el paso
+            anterior (como se muestra continuación)*/
+
+            reportViewer1.LocalReport.EnableExternalImages = true;
+
+            string rutaQR = Application.StartupPath + @"\imagenes\qrcode.png";
+
+            ReportParameter paramImagenQR = new ReportParameter("RutaImagenQR", @"file:\" + rutaQR, true);
+            reportViewer1.LocalReport.SetParameters(paramImagenQR);
+            reportViewer1.RefreshReport();
+
+            this.reportViewer1.RefreshReport();
+            reportViewer1.LocalReport.EnableExternalImages = true;
+
+            string ruta = "";
+
+            if (sala.Nombrevento.Equals("Una Visita Inesperada"))
+            {
+                ruta = Application.StartupPath + @"\imagenes\unavisitainesperada.jpg";
+            }else if(sala.Nombrevento.Equals("La Soga"))
+            {
+                ruta = Application.StartupPath + @"\imagenes\Lasoga.jpg";
+
+            }
+            else if(sala.Nombrevento.Equals("El Cuarto de Veronica"))
+            {
+                ruta = Application.StartupPath + @"\imagenes\cuartovero.jpg";
+
+            }
+
+
+            ReportParameter paramImagen = new ReportParameter("RutaImagenObra", @"file:\" + ruta, true);
+            reportViewer1.LocalReport.SetParameters(paramImagen);
+
+            ReportParameter Titulo = new ReportParameter("RP_Titulo", sala.Nombrevento, true);
+            reportViewer1.LocalReport.SetParameters(Titulo);
+
+            ReportParameter fecha = new ReportParameter("Fecha", sala.Fecha, true);
+            reportViewer1.LocalReport.SetParameters(fecha);
+
+            ReportParameter hora = new ReportParameter("Hora", sala.Hora, true);
+            reportViewer1.LocalReport.SetParameters(hora);
+
+            for (int i = 0; i<DatosAsientos.Count; i++)
+            {
+                precio = 40 + precio;
+            }
+
+            String Sprecio = precio.ToString();
+            ReportParameter precios = new ReportParameter("Precio", Sprecio, true);
+            reportViewer1.LocalReport.SetParameters(precios);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            reportViewer1.LocalReport.DataSources.Add(new ReportDataSource("DataSet1" , DatosAsientos));
+
+
+            reportViewer1.RefreshReport();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            mw.Show();
+        }
+    }
+}
